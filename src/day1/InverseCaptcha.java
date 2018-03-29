750c9ba765106f1915c8936b41ffd573b31bc832
osmanf <osmanf@telenity.com>
2018-03-29 22:32:27 +0300

    day1 second problem solved

--- src/day1/InverseCaptcha.java
+++ src/day1/InverseCaptcha.java
@@ -5,18 +5,34 @@ public class InverseCaptcha {
 	public int process (String captcha) {
 		int result = 0;
 		char[] digits = captcha.toCharArray ();
+		int length = digits.length;
 		int firstChar = Character.getNumericValue (digits[0]);
 		int nextChar;
-		for (int i = 1; i < digits.length;i++) {
-			nextChar = Character.getNumericValue (digits[i]);
+		for (int i = 1; i <= length;i++) {
+			nextChar = Character.getNumericValue (digits[i % length]);
 			if (nextChar == firstChar) {
 				result += firstChar;
 			}
 			firstChar = nextChar;
 		}
-		nextChar = Character.getNumericValue (digits[0]);
-		if (nextChar == firstChar) {
-			result += firstChar;
+
+		return result;
+	}
+
+	public int process2 (String captcha) {
+		int result = 0;
+		char[] digits = captcha.toCharArray ();
+		int length = digits.length;
+		int step = length / 2;
+		int firstChar = Character.getNumericValue (digits[0]);
+		int nextChar, checkChar;
+		for (int i = 1; i <= length;i++) {
+			nextChar = Character.getNumericValue (digits[i % length]);
+			checkChar = Character.getNumericValue (digits[(i - 1 + step) % length]);
+			if (checkChar == firstChar) {
+				result += firstChar;
+			}
+			firstChar = nextChar;
 		}
 
 		return result;
